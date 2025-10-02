519a83ff038b2926743e7fe084362b27c4c4fa2b
Kh1Cuong <ngokhicuong275@gmail.com>
2025-10-02 16:08:22 +0700

    update 2/10/2025

--- src/Components/DisplayInfor.js
+++ src/Components/DisplayInfor.js
@@ -2,12 +2,23 @@ import React from "react";
 
 class DisplayInfor extends React.Component {
     render() {
-        const { age, name } = this.props;
+        // const { age, name } = this.props;
+        // destructuring arr/obj
+        const { listUsers } = this.props
+        console.log(listUsers);
+
         // props => properties
         return (
             <div>
-                <p>My name is {name}</p>
-                <p>My age is {age}</p>
+                {listUsers.map((item) => {
+                    return (
+                        <div key={item.id}>
+                            <p>My name is {item.name}</p>
+                            <p>My age is {item.age}</p>
+                            <hr></hr>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
--- src/Components/MyComponent.js
+++ src/Components/MyComponent.js
@@ -6,15 +6,23 @@ import UserInfor from "./UserInfor";
 import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
+
+  state = {
+    listUsers: [
+      { id: 1, name: "hoidanit1", age: 30 },
+      { id: 2, name: "hoidanit2", age: 25 },
+      { id: 3, name: "hoidanit3", age: 20 }
+
+    ]
+  }
+
   render() {
+    // const array1 = ["a", 1, true, "abc"];
     return (
       <div>
         <UserInfor></UserInfor>
         <br></br>
-        <DisplayInfor name="Ngo Khi Cuong" age="20"></DisplayInfor>
-        <hr></hr>
-        <DisplayInfor name="Hoi dan it" age="30"></DisplayInfor>
-
+        <DisplayInfor listUsers={this.state.listUsers} users={this.state.listUsers}></DisplayInfor>
       </div>
     );
   }
